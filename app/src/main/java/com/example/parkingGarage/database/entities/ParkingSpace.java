b12587a0d5101bd021d84fae0b0f4f2cc1cbc944
gabe-tommi <gabetgal2112@gmail.com>
2025-04-30 10:32:39 -0700

    Added equals, hashcode, getters & setters to ParkingFloor & ParkingSpace object

--- app/src/main/java/com/example/parkingGarage/database/entities/ParkingSpace.java
+++ app/src/main/java/com/example/parkingGarage/database/entities/ParkingSpace.java
@@ -1,5 +1,7 @@
 package com.example.parkingGarage.database.entities;
 
+import java.util.Objects;
+
 public class ParkingSpace{
     private int spaceNum;
     private boolean occupied;
@@ -9,6 +11,51 @@ public class ParkingSpace{
         this.spaceNum = spaceNum;
         this.occupied = occupied;
         this.spaceId = spaceId;
+        this.floorId = floorId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ParkingSpace that = (ParkingSpace) o;
+        return spaceNum == that.spaceNum && occupied == that.occupied && spaceId == that.spaceId && floorId == that.floorId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(spaceNum, occupied, spaceId, floorId);
+    }
+
+    public int getSpaceNum() {
+        return spaceNum;
+    }
+
+    public void setSpaceNum(int spaceNum) {
+        this.spaceNum = spaceNum;
+    }
+
+    public boolean isOccupied() {
+        return occupied;
+    }
+
+    public void setOccupied(boolean occupied) {
+        this.occupied = occupied;
+    }
+
+    public int getSpaceId() {
+        return spaceId;
+    }
+
+    public void setSpaceId(int spaceId) {
+        this.spaceId = spaceId;
+    }
+
+    public int getFloorId() {
+        return floorId;
+    }
+
+    public void setFloorId(int floorId) {
         this.floorId = floorId;
     }
 }
